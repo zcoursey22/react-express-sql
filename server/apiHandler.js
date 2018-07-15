32948e27a905b177a74f6de7aeae4163596af3ed
Zach Coursey <zcoursey22@gmail.com>
2018-07-14 17:27:15 -0700

    Change message on https request

--- server/apiHandler.js
+++ server/apiHandler.js
@@ -6,7 +6,7 @@ let getPark = (parkCode, cb) => {
   let url = `https://developer.nps.gov/api/v1/parks?` + `parkCode=${parkCode}&api_key=${apiKey}`;
 
   https.get(url, (res) => {
-    console.log(res.statusCode);
+    console.log(res.statusCode + ': ' + res.statusMessage);
     var body = '';
 
     res.on('data', (chunk) => {
